554a0fdd1649595a192bff99962c5d1192aac6c5
shyzik93 <shyzik93@mail.ru>
2023-06-19 01:39:17 +0300

    add tests ui for login

--- faci/tests/test_ui.py
+++ faci/tests/test_ui.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.keys import Keys
 
 USERNAME = 'clever_cat'
 PASSWORD = '1234-clever_cat'
+WRONG_LOGIN_MESSAGE = 'Неправильный пароль или пользователь не существует'
 
 
 def populate_step_one(selenium, aim=None, if_not_reached=None):
@@ -27,29 +28,53 @@ def add_member(invited=None, for_what=None):
     pass
 
 
-def test_ui():
-    selenium = webdriver.Chrome()
-
-    # Go to main page
-
-    selenium.get('http://127.0.0.1:8002/')
-
-    # Login
-
+def login(selenium, username, password):
     btn_open_login_form = selenium.find_element('id', 'btn_toggle_login_box')
     field_username = selenium.find_element('id', 'id_for_label_username')
     field_password = selenium.find_element('id', 'id_for_label_password')
     btn_send_login_form = selenium.find_element('id', 'login_button')
 
     btn_open_login_form.send_keys(Keys.ENTER)
-    field_username.send_keys(USERNAME)
-    field_password.send_keys(PASSWORD)
+    field_username.send_keys(username)
+    field_password.send_keys(password)
 
     btn_send_login_form.send_keys(Keys.ENTER)
     time.sleep(2)
 
+
+def test_ui_login():
+    selenium = webdriver.Chrome()
+
+    selenium.get('http://127.0.0.1:8002/')
+
+    login(selenium, USERNAME, PASSWORD)
     with pytest.raises(NoSuchElementException):
         selenium.find_element('id', 'login_bad_message')
+
+    assert selenium.find_element('id', 'logout_button')
+
+
+def test_ui_login_wrong_username():
+    selenium = webdriver.Chrome()
+    selenium.get('http://127.0.0.1:8002/')
+    login(selenium, 'unexisted user', PASSWORD)
+    assert WRONG_LOGIN_MESSAGE in selenium.find_element('id', 'login_bad_message').text
+
+
+def test_ui_login_absent_username():
+    selenium = webdriver.Chrome()
+    selenium.get('http://127.0.0.1:8002/')
+    login(selenium, '', PASSWORD)
+    assert WRONG_LOGIN_MESSAGE in selenium.find_element('id', 'login_bad_message').text
+
+
+def test_ui():
+    selenium = webdriver.Chrome()
+
+    # Go to main page and login
+
+    selenium.get('http://127.0.0.1:8002/')
+    login(selenium, USERNAME, PASSWORD)
 
     # Go to faci list page
 
